c403a0191c52a0ace63c251765c4de5f604665de
Rohit <rohitkumarsahoo038@gmail.com>
2023-09-03 15:54:17 +0530

    bug fixed

--- src/api.js
+++ src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-const auth = JSON.parse(localStorage.getItem("auth"));
+const auth = JSON.parse(localStorage.getItem('auth'))
 export const api = axios.create({
   baseURL: "https://ecommerce-app-backend-cc5o.onrender.com",
-  headers: { "Content-Type": "application/json", authorization: auth?.token },
+  headers: { "Content-Type": "application/json", authorization:auth?.token  },
+
 });
