0cddc590d1353f0e0b1d3702dda9bfe9271ac036
Rohit <rohitkumarsahoo038@gmail.com>
2023-09-03 16:50:47 +0530

    unknow error fix

--- src/components/Routes/AdminRoute.js
+++ src/components/Routes/AdminRoute.js
@@ -8,18 +8,26 @@ import { api } from "../../api";
 export default function AdminRoute() {
   const [ok, setOk] = useState(false);
   const [auth, setAuth] = useAuth();
-
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     const authCheck = async () => {
-      const res = await api.get("/api/v1/auth/admin-auth");
-      if (res.data.ok) {
-        setOk(true);
-      } else {
-        setOk(false);
+      try {
+        setLoading(true);
+        const res = await api.get("/api/v1/auth/admin-auth", {
+          headers: { authorization: localStorage.getItem("auth")?.token },
+        });
+        if (res.data.ok) {
+          setOk(true);
+        } else {
+          setOk(false);
+        }
+      } catch (error) {
+      } finally {
+        setLoading(false);
       }
     };
     if (auth?.token) authCheck();
   }, [auth?.token]);
-
-  return ok ? <Outlet /> : <Spinner path="/dashboard/admin" />;
+  if (loading) return <p>loading...</p>;
+  return ok ? <Outlet /> : <Spinner path="" />;
 }
