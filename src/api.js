b9761c7d869b808fcd0e7745a4a3b4877543e42e
Rohit <rohitkumarsahoo038@gmail.com>
2023-09-14 23:24:41 +0530

    photo fetch corrected

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const auth = JSON.parse(localStorage.getItem("auth"));
 export const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ecommerce-app-backend-cc5o.onrender.com",
   headers: { "Content-Type": "application/json", "authorization": auth?.token },
 });
